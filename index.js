95c92626348bcbbb29eb3ffacebf3f65354a1706
kaue09098eueiei <124529003+kaue09098eueiei@users.noreply.github.com>
2024-10-01 12:33:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const { Collection } = require('discord.js');
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'] });
+
+module.exports = client;
+
+const fs = require('fs');
+client.categories = fs.readdirSync("./commands/");
+client.aliases = new Collection();
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.config = require("./config.json");
+
+require("./handler")(client);
+
+
+
+let db = require("quick.db");
+
+client.userData = new db.table("userData");
+
+client.login(client.config.bot.token);
